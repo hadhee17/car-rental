40a31f70c801ea19f2d0ff64c4a7d89c9b2eced4
hadhee17 <hadhee58@gmail.com>
2025-10-06 12:02:52 +0530

    changes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://drivezy-car-rental-web-application.onrender.com",
+  baseURL: "https://drivezy-car-rental-web-application.onrender.com/",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
